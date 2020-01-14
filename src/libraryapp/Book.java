83180d08cee3b971c03356d6fb054a5ca1d6bfdd
kenstufsz <stanleyekene.ze@gmail.com>
2020-01-13 19:45:55 -0600

    first commit

---
+++ src/libraryapp/Book.java
@@ -0,0 +1,35 @@
+package libraryapp;
+
+public class Book {
+
+    int ISBN =0;
+    String name= "";
+    String author="";
+
+    //isbn, name, description getter and setter method
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+
+}
---
+++ src/libraryapp/LibraryApp.java
@@ -0,0 +1,16 @@
+
+package libraryapp;
+
+
+public class LibraryApp {
+
+    //main method to run the library class
+    public static void main(String[] args) {
+        
+               Library library = new Library(); //creates an object of library class
+        library.setVisible(true);
+
+
+    }
+    
+}
